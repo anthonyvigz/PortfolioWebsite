400f3c87d68323ac8614f5a649c7ca0e759873a6
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-11-26 11:36:46 -0600

    setting up landing page and components

--- main-website/src/App.js
+++ main-website/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom';
+import { useTransition, animated } from 'react-spring';
+import Home from './components/Home';
 import './App.css';
 import './css/landingpage.css';
 
 function App() {
+
+  let location = useLocation();
+
+  let transitions = useTransition(location, location => location.pathname, {
+    from: { opacity: 0, transform: 'translate3d(100%,0,0)' },
+    enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
+    leave: { opacity: 0, transform: 'translate3d(-50%,0,0)' },
+  })
+
   return (
     <div className="App">
       <nav className="navbar">
@@ -16,6 +28,15 @@ function App() {
           <a href="#" >Contact</a>
         </div>
       </nav>
+      <div>
+      <Route exact path="/" component={Home} />
+        {transitions.map(({ item: location, props, key }) => (
+        <animated.div key={key} style={props}>
+          <Switch location={location}>
+          </Switch>
+        </animated.div>
+        ))} 
+    </div>
     </div>
   );
 }
---
+++ main-website/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+
+  return (
+    <div className="homeBlock">
+        <h3>Hello!</h3>
+        <h2>I am Anthony!</h2>
+        <p>
+            I'm a Full Stack Web Developer highly skilled in an array of 
+            front-end and back-end languages and frameworks. 
+            I have a passion for crisp design and flawless functionality.
+        </p>
+    </div>
+  );
+}
+
+export default Home;
