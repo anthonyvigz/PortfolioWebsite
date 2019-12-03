529325e4165dc16282f65c55760990e2e6928055
Anthony Vigliotta <anthonyvigz@gmail.com>
2019-12-03 15:46:51 -0600

    trying to make hover elements on work

--- main-website/src/components/ProjectBlock.jsx
+++ main-website/src/components/ProjectBlock.jsx
@@ -5,10 +5,24 @@ import { Spring } from 'react-spring/renderprops';
 
 function ProjectBlock(props) {
 
+    const [display, setDisplay] = useState({display: "none"})
+
+    const makeVisible = event => {
+        event.preventDefault();
+
+        console.log(display);
+
+        setDisplay({display: "flex", backgroundColor: "red"})
+    }
+
   return (
-    <div>
-        <h1>{props.name}</h1>
-        <h2>{props.link}</h2>
+    <div onMouseEnter={makeVisible} className="mainBlock">
+        <img className="projectImg" src="https://www.howtofascinate.com/wp-content/uploads/Mystique-background-personality-test.jpg" alt="test" width="300px" />
+        <div className="hoverBlock" style={display}>
+            <h1 className="projectTitle">{props.name}</h1>
+            <h2 className="projectDesc">{props.link}</h2>
+            <i className="fa fa-arrow-circle-right" aria-hidden="true"></i>
+        </div>
     </div>
   );
 }
